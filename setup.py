daa760fe83d62beb2c13a09a309266fa69f46627
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2023-01-30 14:27:42 +0800

    Package setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'clck',
-    packages = find_packages(),
+    version="0.1",
+    author="Loui Dominic Naquita",
+    packages = ["clck"],
 )

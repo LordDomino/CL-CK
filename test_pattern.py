f7bde5e36d09b533d21b3cbb9678d24e2c4a0acb
LordDomino <louidominicnaquita16@gmail.com>
2023-05-27 23:30:20 +0800

    Sync

--- test_pattern.py
+++ test_pattern.py
@@ -1,7 +1,5 @@
-from clck.buildingblocks.emics import Phoneme
 from clck.buildingblocks.patterns import Pattern
-from clck.buildingblocks.inventories import Inventory, Cluster
 
-pattern = Pattern("myPattern", "[E|A|B]i.o.i|O.h.io")
+pattern: Pattern = Pattern("myPattern", "[E|A|B]i.o.i|O.h.io")
 
 print(str(["a"]))

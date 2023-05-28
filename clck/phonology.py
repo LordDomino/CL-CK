bed903be11b4e72643ef43561bfc29e9c33c4e32
LordDomino <louidominicnaquita16@gmail.com>
2023-05-28 23:25:23 +0800

    Completed adding templates for all vowels and all pulmonic consonants

--- clck/phonology.py
+++ clck/phonology.py
@@ -3,102 +3,119 @@ from typing import Literal
 
 
 
-@dataclass
 class IPA_Phoneme:
-  string: str
-  place: str
-  manner: str
+  def __init__(self, string: str, place: str, manner: str) -> None:
+    self._string: str = string
+    self._place: str = place
+    self._manner: str = manner
+
+  def __str__(self) -> str:
+    return self._string
+
+  def __repr__(self) -> str:
+    return f"<Phoneme {self._string}>"
 
 
 
-@dataclass
 class IPA_Phoneme_PulmonicConsonant(IPA_Phoneme):
-  string: str
-  place: Literal[
-    "bilabial",
-    "labiodental",
-    "linguolabial",
-    "dental",
-    "alveolar",
-    "postalveolar",
-    "retroflex",
-    "palatal",
-    "velar",
-    "uvular",
-    "epiglottal",
-    "glottal",
-  ]
-  manner: Literal[
-    "nasal",
-    "plosive",
-    "sibilant affricate",
-    "nonsibilant affricate",
-    "sibilant fricative",
-    "nonsibilant fricative",
-    "approximanrt",
-    "tap",
-    "trill",
-    "lateral affricate",
-    "lateral fricative",
-    "lateral approximant",
-    "lateral tap",
-  ]
-  voiced: bool
+  def __init__(
+      self,
+      string: str,
+      place: Literal[
+        "bilabial",
+        "labiodental",
+        "linguolabial",
+        "dental",
+        "alveolar",
+        "postalveolar",
+        "retroflex",
+        "palatal",
+        "velar",
+        "uvular",
+        "epiglottal",
+        "glottal",
+      ],
+      manner: Literal[
+        "nasal",
+        "plosive",
+        "sibilant affricate",
+        "nonsibilant affricate",
+        "sibilant fricative",
+        "nonsibilant fricative",
+        "approximant",
+        "tap",
+        "trill",
+        "lateral affricate",
+        "lateral fricative",
+        "lateral approximant",
+        "lateral tap",
+      ],
+      voiced: bool
+  ) -> None:
+    super().__init__(string, place, manner)
+    self._voiced: bool = voiced
 
 
 
-@dataclass
 class IPA_Phoneme_NonpulmonicConsonant(IPA_Phoneme):
-  string: str
-  place: Literal[
-    "bilabial",
-    "labiodental",
-    "linguolabial",
-    "dental",
-    "alveolar",
-    "postalveolar",
-    "retroflex",
-    "palatal",
-    "velar",
-    "uvular",
-    "epiglottal",
-  ]
-  manner: Literal[
-    "ejective stop",
-    "ejective affricate",
-    "ejective fractive",
-    "ejective lateral affricate",
-    "ejective lateral fricative",
-    "click tenuis",
-    "click voiced",
-    "click nasal",
-    "click tenuis lateral",
-    "click voiced lateral",
-    "click nasal lateral",
-    "implosive voiced",
-    "implosive voiceless",
-  ]
+  def __init__(
+      self,
+      string: str,
+      place: Literal[
+        "bilabial",
+        "labiodental",
+        "linguolabial",
+        "dental",
+        "alveolar",
+        "postalveolar",
+        "retroflex",
+        "palatal",
+        "velar",
+        "uvular",
+        "epiglottal",
+      ],
+      manner: Literal[
+        "ejective stop",
+        "ejective affricate",
+        "ejective fractive",
+        "ejective lateral affricate",
+        "ejective lateral fricative",
+        "click tenuis",
+        "click voiced",
+        "click nasal",
+        "click tenuis lateral",
+        "click voiced lateral",
+        "click nasal lateral",
+        "implosive voiced",
+        "implosive voiceless",
+      ]
+  ) -> None:
+    super().__init__(string, place, manner)
 
 
 
 @dataclass
 class IPA_Phoneme_Vowel(IPA_Phoneme):
-  string: str
-  place: Literal[
-    "front",
-    "central",
-    "back",
-  ]
-  manner: Literal[
-    "close",
-    "nearclose",
-    "closemid",
-    "mid",
-    "openmid",
-    "nearopen",
-    "open",
-  ]
-  rounded: bool
+  def __init__(
+      self,
+      string: str,
+      place: Literal[
+        "front",
+        "central",
+        "back",
+      ],
+      manner: Literal[
+        "close",
+        "nearclose",
+        "closemid",
+        "mid",
+        "openmid",
+        "nearopen",
+        "open",
+      ],
+      rounded: bool | None
+  ) -> None:
+    super().__init__(string, place, manner)
 
 
 

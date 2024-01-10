a9486c28bf8506117f0f9fc1a48f0ba32b9ef92f
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2024-01-10 20:49:14 +0800

    Documentation

--- clck/common/component.py
+++ clck/common/component.py
@@ -5,11 +5,19 @@ from clck.config import print_debug
 
 
 class Component(ABC):
-    """The class representing all linguistic component.
+    """The class representing all abstract representations of linguistic
+    objects from which most classes of CLCK inherit from.
     
-    A linguistic component is an abstract representation of a unit of
-    language which is transcriptable (contains an IPA transcript) and
-    can be outputted as a string that is readable by the user.
+    In CLCK, these linguistic objects are the building blocks which
+    operate and are operated upon to form various constructs and
+    features of a language. As being an abstract representation of a
+    unit of language, `Component`s are always transcriptable and can be
+    directly previewed as its output string.
+
+    `Component`s contain several base properties which are initialized
+    upon from a subclass super call. The base properties are `output`,
+    `ipa_transcript`, `formulang_transcript`, `romanization`, and
+    `blueprint`.
     """
 
     @abstractmethod
@@ -21,6 +29,7 @@ class Component(ABC):
         self._ipa_transcript: str = self._init_ipa_transcript()
         self._formulang_transcript: str = self._init_formulang_transcript()
         self._romanization: str | None = self._init_romanization()
+        self._blueprint = self._init_component_blueprint()
 
         print_debug(f"{self} base properties initialized")
 
@@ -38,12 +47,16 @@ class Component(ABC):
 
     @abstractmethod
     def _init_ipa_transcript(self) -> str:
-        """
-        Creates and returns the IPA transcript for this component.
-        
+        """Initializes and returns the IPA transcription of this
+        component.
+
+        The string representation of how this component might be
+        transcribed as an IPA phonemic transcription.
+
         Returns
         -------
-        - The IPA transcript for this component.
+        str
+            the IPA transcription of this component.
         """
         pass
 
@@ -53,12 +66,15 @@ class Component(ABC):
 
     @abstractmethod
     def _init_output(self) -> str:
-        """
-        Creates and returns the output string of this component.
+        """Initializes and returns the output string of this component.
+        
+        The output string of a component is its printable string version
+        that previews the 'actual' orthographic representation of it.
 
         Returns
         -------
-        - The output string of this component.
+        str
+            the output string of this component.
         """
         pass
 
@@ -66,14 +82,22 @@ class Component(ABC):
     def _init_romanization(self) -> str | None:
         pass
 
+    @abstractmethod
+    def _init_component_blueprint(self) -> "ComponentBlueprint":
+        pass
+
     @property
     def output(self) -> str:
-        """The final string representation of this component."""
+        """The printable string version that previews the 'actual'
+        orthographic representation of this component.
+        """
         return self._output
 
     @property
     def ipa_transcript(self) -> str:
-        """The IPA transcript of this component."""
+        """The string representation of how this component might be
+        transcribed as an IPA phonemic transcription.
+        """
         return self._ipa_transcript
     
     @property
@@ -89,3 +113,7 @@ class Component(ABC):
                 the string to set this component's romanization
         """
         self._romanization = romanization
+
+
+class ComponentBlueprint:
+    pass
